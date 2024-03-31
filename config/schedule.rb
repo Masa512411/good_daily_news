0b5ec611aee9dd13008df2dd1cc6e361e5efa2d2
Masa512411 <masa14586@gmail.com>
2024-03-31 17:26:56 +0900

    render.ymlの追加

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+
+set :output, "#{Rails.root}/log/cron_tab.log"
+
 every 10.minutes do #at: '9:00 am' do
   rake "fetch_news:FetchNews"
 end
