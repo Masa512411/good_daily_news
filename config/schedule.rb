83f6148442d957d535b657b287c21ceb1a8a9fbf
Masa512411 <masa14586@gmail.com>
2024-04-14 23:50:29 +0900

    supabaseに変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 
 set :output, "#{Rails.root}/log/cron_tab.log"
+rails_env = ENV['RAILS_ENV'] || :development
+set :environment, rails_env
+env :PATH, ENV['PATH']
 
-every 10.minutes do #at: '9:00 am' do
+every 1.minutes do #at: '9:00 am' do
   rake "fetch_news:FetchNews"
 end
